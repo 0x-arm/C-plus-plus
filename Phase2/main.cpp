98f36fa11e27c7a55d4d4c81a7960a37a4907ff8
0x-arm <armenien0777@gmail.com>
2025-04-06 17:22:41 +0200

    AJOUT COMMENTAIRES

--- Phase2/main.cpp
+++ Phase2/main.cpp
@@ -3,184 +3,313 @@
 #include <cmath> // module pour les fonctions mathématiques ( dans ce code : std::min()  et std::max() )
 #include <vector> // module pour les vecteurs
 #include <tuple> // module pour les tuples
-#include <cctype>
-#include <unordered_map>
-#include "memory.hpp" // module pour la gestion de la mémoire
+#include <cctype> // module pour la gestion des caractères (dans ce code : std::isdigit() et std::isalpha())
+#include <unordered_map> // module pour le dictionnaire des registres (dans ce code : std::unordered_map<std::string, int>)
+#include "memory.hpp" // module pour la gestion de la mémoire (importation de la mémoire dans le fichier 'memory.hpp')
 
+
+//// Retourne la valeur saturée d'un entier
 uint16_t saturated(int n) {
 
-    constexpr int MAX_UPPER_LIMIT = 65535; // Valeur maximale du registre
-    uint16_t value = std::min(std::max(n, 0), MAX_UPPER_LIMIT);
+	// Valeur maximale du registre
+    constexpr int MAX_UPPER_LIMIT = 65535;
+
+	// Saturation de la valeur
+    uint16_t value = std::min(std::max(n, 0), MAX_UPPER_LIMIT); 
 	
+	// Retourner la valeur saturée
 	return value;
 
 }
 
-// Retourne les mots de l'instruction sous forme d'un vecteur
+
+//// Retourne les mots de l'instruction sous forme d'un vecteur
 std::vector<std::string> parse_instr(const std::string& instr) {
 
+	// Vecteur pour contenir les mots de l'instruction
     std::vector<std::string> elements;
+
+	// Variable pour stocker le mot en cours de traitement
     std::string value;
+
+	// Variables utilisées pour le traitement de l'inscrution (indice = i, taille de l'instruction = s)
     int i = 0, s = instr.size();
 
+	// Parcourir l'instruction
     while (i <= s) {
+
+		// Si on trouve un espace ou la fin de l'instruction
         if (instr[i] == ' ' || instr[i] == '\0') {
+
+			// Si le mot en cours de traitement n'est pas vide
             if (!value.empty()) {
+
+				// Ajouter le mot au vecteur
                 elements.push_back(value);
+
+				// Réinitialiser le mot en cours de traitement
                 value = "";
             }
-        } 
+        }
         else {
+
+			// Sinon ajouter le caractère au mot en cours de traitement
             value += instr[i];
         }
 
+		// Incrémenter pour passer au caractère suivant
 		i++;
 	}
 
+	// Retourner le vecteur contenant les mots de l'instruction
     return elements;
 }
 
-// Retourne l'instruction sans les opérandes
+
+//// Retourne l'instruction sans les opérandes
 std::string parse_opcode(const std::string& instr) {
 
+	// Récupérer les mots de l'instruction grâce à la fonction 'parse_instr'
 	std::vector<std::string> elements = parse_instr(instr);
+
+	// Le premier mot de du vecteur est l'opcode
 	std::string opcode = elements.at(0);
 
+	// Retourner l'opcode
 	return opcode;
 }
 
-// Retourne les opérandes (paramètres) sous forme de tuple de strings
+
+//// Retourne les opérandes (paramètres) sous forme de tuple de strings
 std::tuple<std::string, std::string> parse_operands(const std::string& instr) {
+
+	// Récupérer les mots de l'instruction grâce à la fonction 'parse_instr'
     std::vector<std::string> elements = parse_instr(instr);
+
+	// Récuperer le nombre d'éléments dans le vecteur
 	int s = elements.size();
 
+
+	// Définitions des variables pour stocker les opérandes
     static std::string first_operand;
     static std::string second_operand;
 
+	// Si le vecteur contient au moins 2 éléments, on récupère les opérandes
 	if (s > 1) {
+
+		// Récupérer le premier opérande
         first_operand = elements.at(1);
 		
+		// Si le vecteur contient au moins 3 éléments, on récupère le deuxième opérande
 		if (s > 2) {
+
+			// Récupérer le deuxième opérande
 			second_operand = elements.at(2);
 		}
     }
 	
+	// Retourner les opérandes sous forme de tuple
     return {first_operand, second_operand};
 }
 
-// Exécute l'instruction
+
+//// Exécute l'instruction
 void exec_instr(const std::string& opcode, const std::string& first_operand, const std::string& second_operand, std::unordered_map<std::string, int>& registers, bool& skip) {
 
+	// Si l'opcode est IFNZ
 	if (opcode == "IFNZ") {
+
+		// Vérifier si le registre est égal à 0
 		if (registers[first_operand] == 0) {
+
+			// Si oui, on saute l'instruction suivante (skip = true)
 			skip = true;
 		}
 	}
 
+
+	// Si l'opcode est SET
 	else if (opcode == "SET") {
+
+		// Si le premier opérande est une valeur
 		if (std::isdigit(second_operand[0])) {
+
+			// Convertir en entier et le stocker dans le registre
 			registers[first_operand] = saturated(std::stoi(second_operand));
 		}
+
+		// Sinon le premier opérande est un registre
 		else {
+
+			// Copier la valeur du registre dans l'autre registre
 			registers[first_operand] = saturated(registers[second_operand]);
 		}
 	}
 
+	// Si l'opcode est ADD
 	else if (opcode == "ADD") {
+
+		// Si le second opérande est une valeur
 		if (std::isdigit(second_operand[0])) {
+
+			// Additioner la valeur avec le registre donnant un résultat saturé et mettre à jour le registre par ce résultat
 			registers[first_operand] = saturated(registers[first_operand] + std::stoi(second_operand));
 		}
+
+		// Sinon le second opérande est un registre
 		else {
+
+			// Additioner les deux registres donnant un résultat saturé et mettre à jour le premier registre par ce résultat
 			registers[first_operand] = saturated(registers[first_operand] + registers[second_operand]);
 		}
 	}
 
+	// Si l'opcode est SUB
 	else if (opcode == "SUB") {
+
+		// Si le second opérande est une valeur
 		if (std::isdigit(second_operand[0])) {
+
+			// Soustraire la valeur avec le registre donnant un résultat saturé et mettre à jour le registre par ce résultat
 			registers[first_operand] = saturated(registers[first_operand] - std::stoi(second_operand));
 		}
+
+		// Sinon le second opérande est un registre
 		else {
+
+			// Soustraire les deux registres donnant un résultat saturé et mettre à jour le premier registre par ce résultat
 			registers[first_operand] = saturated(registers[first_operand] - registers[second_operand]);
 		}
 	}
 
+	// Si l'opcode est PRINT
 	else if (opcode == "PRINT") {
+
+		// Afficher la valeur du registre
 		std::cout << registers[first_operand] << std::endl;
 	}
 
+	// Si l'opcode est STORE
 	else if (opcode == "STORE") {
+
+		// Si le premier opérande est une valeur
 		if (std::isdigit(first_operand[0])) {
+
+			// Convertir le premier opérande en entier non signé de 8 bits, qui repésente l'adresse mémoire
 			uint8_t address = std::stoi(first_operand);
+
+			// Ecriture de la valeur dans l'adresse mémoire
 			write(address, registers[second_operand]);
 		}
 	}
 
+	// Si l'opcode est LOAD
 	else if (opcode == "LOAD") {
+
+		// Si le premier opérande est une valeur
 		if (std::isdigit(first_operand[0])) {
+
+			// Convertir le premier opérande en entier non signé de 8 bits, qui repésente l'adresse mémoire
 			uint8_t address = std::stoi(first_operand);
+
+			// Lire la valeur de l'adresse mémoire et récupérer cette valeur
 			uint16_t data = read(address);
+
+			// Stocker la valeur dans le registre
 			registers[second_operand] = data;
 		}
 	}
 
+	// Si l'opcode est PUSH
 	else if (opcode == "PUSH") {
+
+		// Si le premier opérande est un charactère
 		if (std::isalpha(first_operand[0])) {
+
+			// Convertir le registre en entier non signé de 16 bits
 			uint16_t value = registers[first_operand];
+
+			// Push la valeur sur la pile
 			push(value);
 		}
 	}
 
+	// Si l'opcode est POP
 	else if (opcode == "POP") {
+
+		// Si le premier opérande est un charactère
 		if (std::isalpha(first_operand[0])) {
+			// Pop la valeur de la pile et récupérer cette valeur
 			uint16_t data = pop();
+
+			// Stocker la valeur dans le registre
 			registers[first_operand] = data;
 		}
 	}
 
 }
 
+
 // Exécute le programme dans le fichier texte' program_path'
 void exec(const std::string& program_path) {
 
+	// Initialisation des registres (a, b, c, d) à 0
 	static std::unordered_map<std::string, int> registers = {
 		{"a", 0},
 		{"b", 0},
 		{"c", 0},
 		{"d", 0}
-	}; // initialisation du registre
+	};
 
-	static bool skip = false; // variable nécessaire à l'instruction 'IFNZ'
+	// Initialisation de la variable 'skip' à false (pour sauter l'instruction suivante si nécessaire)
+	static bool skip = false;
 
+	// Ouverture du fichier contenant le programme
 	static std::ifstream file(program_path);
 
+	// Définition de la variable pour stocker l'instruction lue
     std::string instr;
 
+	// Parcourir le fichier ligne par ligne
     while (getline(file, instr)) {
 
+
+		// Si l'instruction ne doit pas être passée (skip = false)
 		if (!skip) {
 
+			// Récupération de l'opcode
 			std::string opcode = parse_opcode(instr);
+
+			// Récupération des opérandes
 			std::tuple<std::string, std::string> operand = parse_operands(instr);
 
+			// Récupération séparée des opérandes
 			std::string first_operand = std::get<0>(operand);
 			std::string second_operand = std::get<1>(operand);
 
+			// Exécuter l'instruction
 			exec_instr(opcode, first_operand, second_operand, registers, skip); // exécution de l'instruction
 
 		}
 
+		// Si l'instruction l'instruction n'est pas lue (skip = true)
 		else {
-			skip = false; // réinitialisation de la variable 'skip' pour la prochaine instruction
+
+			// Réinitialisation de la variable 'skip' pour la prochaine instruction
+			skip = false;
 		};
 
 	};
 
+	// Fermeture du fichier
 	file.close();
 }
 
 // Fonction Main
 int main() {
 
-	exec("program.txt"); // Exécution du programme sur le fichier 'program.txt'
+	// Exécution du programme sur le fichier 'program.txt'
+	exec("program.txt");
+
 	return 0; // Retourne rien si tout est OK
 }
--- Phase2/memory.cpp
+++ Phase2/memory.cpp
@@ -8,38 +8,59 @@ uint8_t memory[256];
 // Pointeur de pile (Stack Pointer) initialisé à 0
 uint8_t SP = 0;
 
-// Fonction de lecture (read) : lit 2 octets consécutifs (16 bits) en little endian
+//// Fonction de lecture (read) : lit 2 octets consécutifs (16 bits) en little endian
 uint16_t read(uint8_t address) {
-    constexpr int MAX_MEMORY = 256; // Taille de la mémoire
+    
+    // Taille de la mémoire
+    constexpr int MAX_MEMORY = 256;
+
+    // Retourner la valeur lue saturée à l'adresse mémoire spécifiée (octet de poids faible + octet de poids fort)
     return std::min(std::max((memory[address] | (memory[address + 1] << 8)), 0), MAX_MEMORY); // seulement memory[address] | (memory[address + 1] << 8) ==> 33304 car non-borné
 }
 
-// Fonction d’écriture (write) : écrit 2 octets consécutifs (16 bits) en little endian
+//// Fonction d’écriture (write) : écrit 2 octets consécutifs (16 bits) en little endian
 void write(uint8_t address, uint16_t value) {
-    memory[address] = value & 0xFF;           // Octet de poids faible
+
+    // Octet de poids faible
+    memory[address] = value & 0xFF;
+
+    // Octet de poids fort
     memory[address + 1] = (value >> 8);
 }
 
-// Ajoute une valeur 16 bits au sommet de la pile
+//// Ajoute une valeur 16 bits au sommet de la pile
 void push(uint16_t value) {
     
-    // Vérifie si la pile déborde (au-delà de l’adresse 15)
+    // Vérifier si la pile déborde (au-delà de l’adresse 15)
     if (SP + 1 >= 16) {
+
+        // Quitter le programme
         std::exit(1);
     }
 
-    memory[SP] = value & 0xFF;                // Octet de poids faible
-    memory[SP + 1] = (value >> 8);            // Octet de poids fort
+    // Récupérer l'octet de poids faible
+    memory[SP] = value & 0xFF;
+
+    // Récupérer l'octet de poids fort
+    memory[SP + 1] = (value >> 8);
+
+    // Incrémenter le pointeur de pile
     SP += 2;
 }
 
-// Retire une valeur 16 bits du sommet de la pile et la retourne 
+//// Retire une valeur 16 bits du sommet de la pile et la retourne 
 uint16_t pop() {
-    // Vérifie si la pile est vide
+
+    // Vérifier si la pile est vide
     if (SP < 2) {
+
+        // Quitter le programme
         std::exit(1); 
     }
 
+    // Décrémenter le pointeur de pile
     SP -= 2;
+
+    // Retourner la valeur lue de la pile (octet de poids faible + octet de poids fort)
     return memory[SP] | (memory[SP + 1] << 8);
 }
