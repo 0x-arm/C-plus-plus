0688eb9a30b82c4907bcf28def4def729091919d
0x-arm <armenien0777@gmail.com>
2025-04-08 16:05:44 +0200

    MAJ

--- Phase2/memory.cpp
+++ Phase2/memory.cpp
@@ -2,51 +2,58 @@
 #include <cstdlib>
 #include "Memory.hpp"
 
+// Taille de la mémoire
+constexpr int MAX_MEMORY = 256;
+
+// Taille de la pile
+constexpr int MAX_STACK = 16;
+
+// Taille d'un octet (8 bits)
+constexpr int OCTES_SIZE = 8;
+
 // Constructeur : initialise la mémoire et le pointeur de pile
 Memory::Memory() {
-    for (int i = 0; i < 256; ++i) {
+    for (int i = 0; i < MAX_MEMORY; ++i) {
         _memory[i] = 0;
     }
 }
 
-//// Méthode de lecture (read) : lit 2 octets consécutifs (16 bits) en little endian
+//// Méthode qui lit une valeur sur 16 bits dans la mémoire en little endian
 uint16_t Memory::read(uint8_t address) {
-    
-    // Taille de la mémoire
-    constexpr int MAX_MEMORY = 256;
 
     // Récupération de la valeur saturée à l'adresse mémoire spécifiée (octet de poids faible + octet de poids fort)
-    uint16_t value = std::min(std::max((_memory[address] | (_memory[address + 1] << 8)), 0), MAX_MEMORY);
+    uint16_t value = std::min(std::max((_memory[address] | (_memory[address + 1] << OCTES_SIZE)), 0), MAX_MEMORY);
 
     // Retourner la valeur lue saturée
     return value; // seulement memory[address] | (memory[address + 1] << 8) ==> résultat = 33304 car value = non borné/saturée
 }
 
-//// Méthode d’écriture (write) : écrit 2 octets consécutifs (16 bits) en little endian
+//// Méthode qui écrit une valeur sur 16 bits dans la mémoire en little endian
 void Memory::write(uint8_t address, uint16_t value) {
 
     // Octet de poids faible
-    _memory[address] = value & 0xFF;
+    _memory[address] = static_cast<uint8_t>(value);
 
     // Octet de poids fort
-    _memory[address + 1] = (value >> 8);
+    _memory[address + 1] = static_cast<uint8_t>(value >> OCTES_SIZE);
+
 }
 
 //// Méthode qui ajoute une valeur 16 bits au sommet de la pile
 void Memory::push(uint16_t value) {
     
     // Vérifier si la pile déborde (au-delà de l’adresse 15)
-    if (_SP + 1 >= 16) {
+    if (_SP + 1 >= MAX_STACK) {
 
         // Quitter le programme
         std::exit(1);
     }
 
     // Récupérer l'octet de poids faible
-    _memory[_SP] = value & 0xFF;
+    _memory[_SP] = static_cast<uint8_t>(value);
 
     // Récupérer l'octet de poids fort
-    _memory[_SP + 1] = (value >> 8);
+    _memory[_SP + 1] = static_cast<uint8_t>(value >> OCTES_SIZE);
 
     // Incrémenter le pointeur de pile
     _SP += 2;
@@ -66,6 +73,6 @@ uint16_t Memory::pop() {
     _SP -= 2;
 
     // Retourner la valeur lue de la pile (octet de poids faible + octet de poids fort)
-    return _memory[_SP] | (_memory[_SP + 1] << 8);
+    return _memory[_SP] | (_memory[_SP + 1] << OCTES_SIZE);
 
 }
